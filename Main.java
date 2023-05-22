9c58261683a64028096f288e3507dcd38cfec317
lmontielv <luis.montiel@hotmail.com>
2023-05-22 17:20:57 -0400

    tarea de encapculacion

---
+++ Main.java
@@ -0,0 +1,26 @@
+/* TAREA DE SECCIÓN 8 ENCAPSULACIÓN 
+Crear un objeto persona en el main.
+Utiliza los gets y sets para darle valores a las propiedades edad, nombre y teléfono, 
+por último muéstralas por consola.
+*/
+
+public class Main {
+    
+
+    public static void main(String[] args) {
+
+        Persona persona = new Persona();
+
+        persona.setNombre("Luis Montiel");
+        persona.setEdad(54);
+        persona.setTelefono("+584246398596");
+
+        System.out.println("El nombre de la persona es :");
+        System.out.println(persona.getNombre());
+        System.out.println("Su edad es :");
+        System.out.println(persona.getEdad());
+        System.out.println("Su número de teléfono es : ");
+        System.out.println(persona.getTelefono());
+
+    }
+}
---
+++ Persona.java
@@ -0,0 +1,36 @@
+/* TAREA DE SECCIÓN 8 ENCAPSULACIÓN 
+Crear clase Persona.
+Crear variables las privadas edad, nombre y telefono de la clase Persona.
+Crear gets y sets de cada propiedad.
+*/
+
+class Persona {
+    private String nombre;
+    private int edad;
+    private String telefono;
+
+
+    public void setNombre(String nombre){
+        this.nombre= nombre;
+    }
+
+    public void setEdad(int edad){
+        this.edad= edad;
+    }
+
+    public void setTelefono(String telefono){
+        this.telefono = telefono;
+    }
+
+    public String getNombre(){
+        return this.nombre;
+    }
+
+    public int getEdad(){
+        return this.edad;
+    }
+
+    public String getTelefono(){
+        return this.telefono;
+    }
+}
